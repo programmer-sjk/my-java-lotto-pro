2294c5887dea1101df2f4a381e1b5606a2863c01
sjk5766 <wjdrnr6662@gmail.com>
2022-10-26 21:19:04 +0900

    요구사항 2. 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -8,13 +8,14 @@ import java.util.stream.Collectors;
 
 public class StringAddCalculator {
     public static final int DEFAULT = 0;
+    public static final String DELIMITER = ",|:";
 
     public static int splitAndSum(String input) {
         if (isEmpty(input)) {
             return DEFAULT;
         }
 
-        return 1;
+        return sum(parse(split(input)));
     }
 
     private static boolean isEmpty(String input) {
@@ -23,5 +24,20 @@ public class StringAddCalculator {
         }
 
         return false;
+    }
+
+    private static String[] split(String input) {
+        return input.split(DELIMITER);
+    }
+
+    private static List<Integer> parse(String[] inputs) {
+        return Arrays.stream(inputs)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static int sum(List<Integer> numbers) {
+        return numbers.stream()
+                .reduce(DEFAULT, (a, b) -> a + b);
     }
 }
